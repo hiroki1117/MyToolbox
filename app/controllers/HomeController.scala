2ace67fa63a898ed82e4a018c243abdcef4e9671
hiroki <scala1117@gmail.com>
2019-01-28 18:14:00 +0900

    認証処理の流れを実装

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,10 +3,19 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.i18n.I18nSupport
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport{
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
+  }
+
+  def login() = Action {implicit request =>
+    if(request.method=="POST") {
+      Redirect(routes.HomeController.index())
+    } else {
+      Ok(views.html.login())
+    }
   }
 }
