28cff21cab7396441512e793b5119c4497507f6c
hiroki <scala1117@gmail.com>
2019-01-31 08:28:24 +0900

    ApplicationControllerのActionをloginActionに修正

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -3,13 +3,14 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import com.danielasfregola.twitter4s.TwitterRestClient
+import util.LoginConfirmAction
 
-class ApplicationController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class ApplicationController @Inject()(loginAction: LoginConfirmAction, cc: ControllerComponents) extends AbstractController(cc) {
   val client = TwitterRestClient()
 
-  def myTweet() = Action{ Ok(views.html.mytweet()) }
+  def myTweet() = loginAction{ Ok(views.html.mytweet()) }
 
-  def postTweet() = Action{implicit request =>
+  def postTweet() = loginAction{implicit request =>
     val data = request.body.asFormUrlEncoded.get("tweet")
     client.createTweet(status=data(0))
     Ok(views.html.mytweet())
