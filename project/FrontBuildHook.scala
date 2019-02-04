75882389357ef35ad9ec27570d6d2663bab35cd4
hiroki <scala1117@gmail.com>
2019-02-04 16:36:19 +0900

    frontをビルドするHookを実装

---
+++ project/FrontBuildHook.scala
@@ -0,0 +1,23 @@
+import java.net.InetSocketAddress
+import play.sbt.PlayRunHook
+import sbt._
+import scala.sys.process.Process
+
+object FrontBuildHook {
+  def apply(base: File): PlayRunHook = {
+    object FrontBuildProcess extends PlayRunHook {
+      var process: Option[Process] = None
+
+      override def beforeStarted(): Unit = {}
+
+      override def afterStarted(addr: InetSocketAddress): Unit = {
+        process = Option(Process(Seq("cmd", "/c", "npm run build"), base / "front").run)
+      }
+
+      override def afterStopped(): Unit = {
+        process.foreach(_.destroy())
+      }
+    }
+    FrontBuildProcess
+  }
+}
