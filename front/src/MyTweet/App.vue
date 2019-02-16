d2d91f60d893e639f539e9720a496bf590c42900
hiroki <scala1117@gmail.com>
2019-02-16 22:49:41 +0900

    mwtweetのフロント雛形

---
+++ front/src/MyTweet/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>MyTweet</h1>
+  </div>
+</template>
--- front/vue.config.js
+++ front/vue.config.js
@@ -1,8 +1,20 @@
 module.exports = {
-  outputDir: '../public/vue',
+  outputDir: "../public/vue",
   filenameHashing: false,
   productionSourceMap: false,
   css: {
     extract: false
+  },
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html"
+    },
+    mytweet: {
+      entry: "src/MyTweet/main.js",
+      template: "public/index.html",
+      filename: "mytweet.html"
+    }
   }
 };
