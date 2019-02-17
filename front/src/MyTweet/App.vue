394129cc68e9134a8bbeb4ec84737bc1a243f47a
hiroki <scala1117@gmail.com>
2019-02-17 09:23:52 +0900

    frontのaxiosでの通信

--- front/src/MyTweet/App.vue
+++ front/src/MyTweet/App.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    <h1>MyTweet</h1>
+    <textarea v-model="tweet"></textarea>
+    <button @click="send">send</button>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      tweet: ""
+    };
+  },
+  methods: {
+    send: function() {
+      alert(this.tweet);
+      axios
+        .post("/mytweet/api/post", { tweet: this.tweet })
+        .then(response => console.log(response.data));
+      this.tweet = "";
+    }
+  }
+};
+</script>
