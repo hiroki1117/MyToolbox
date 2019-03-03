eb4793aa1f01121193e5211f90cb8ad57d755eaf
hiroki ohno <scala1117@gmail.com>
2019-03-04 07:56:37 +0900

    import文の修正

--- app/util/LoginConfirmAction.scala
+++ app/util/LoginConfirmAction.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
-import play.api.mvc.ControllerHelpers._
+import play.api.mvc.ControllerHelpers.Redirect
 
 class LoginConfirmAction @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
   override def invokeBlock[A](request: Request[A], block: (Request[A] => Future[Result])): Future[Result] = {
